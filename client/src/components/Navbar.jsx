d68db780df2f55345088cfd33c67ec06f80281a8
shinhonggyu <zowoz0@naver.com>
2021-09-05 19:24:49 +0900

    client complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Badge } from '@material-ui/core';
 import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: '50px' })}
 `;
 
 const Wrapper = styled.div`
@@ -11,6 +13,9 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${mobile({
+    padding: '10px 0px',
+  })}
 `;
 
 const Left = styled.div`
@@ -22,6 +27,7 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: 'none' })}
 `;
 
 const SearchContainer = styled.div`
@@ -34,6 +40,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: '50px' })}
   &:focus {
     outline: none;
   }
@@ -46,6 +53,7 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: '24px' })}
 `;
 
 const Right = styled.div`
@@ -53,12 +61,14 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ justifyContent: 'center', flex: 2 })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: '12px', marginLeft: '10px' })}
 `;
 
 const Navbar = () => {
@@ -68,7 +78,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: 'gray', fontSize: '18px' }} />
           </SearchContainer>
         </Left>
