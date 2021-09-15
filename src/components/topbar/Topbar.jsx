b71be1e35e38276b6d3bd696d27eb01e59139d3a
shinhonggyu <zowoz0@naver.com>
2021-09-15 22:24:36 +0900

    admin

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,37 @@
+import { Language, NotificationsNone, Settings } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import './topbar.css';
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <Link to="/" className="link">
+            <span className="logo">lamaadmin</span>
+          </Link>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBag">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBag">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
