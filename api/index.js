2cbdbd3f6e6d4ea6c95c7ba17f099fc326629ba4
shinhonggyu <zowoz0@naver.com>
2021-09-21 18:13:46 +0900

    node rest api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const productRoute = require('./routes/products');
+const cartsRoute = require('./routes/carts');
+const ordersRoute = require('./routes/orders');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -10,6 +17,15 @@ mongoose
   .then(() => console.log('DBConnection Successfull!'))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => {
+app.use(cors());
+app.use(express.json());
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartsRoute);
+app.use('/api/orders', ordersRoute);
+app.use('/api/checkout', stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!');
 });
---
+++ api/models/Cart.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+// for Carts API CRUD
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Cart', CartSchema);
