0bd2d2542565f65cdb93ae4cc3e2cf0465e0af30
shinhonggyu <zowoz0@naver.com>
2021-09-24 03:49:25 +0900

    up to date

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,35 +1,36 @@
-import {
-  FavoriteBorderOutlined,
-  SearchOutlined,
-  ShoppingCartOutlined,
-} from '@material-ui/icons';
+import { SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons';
 import styled from 'styled-components';
 
 const Info = styled.div`
   opacity: 0;
+  position: absolute;
   width: 100%;
   height: 100%;
-  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   top: 0;
   left: 0;
   background-color: rgba(0, 0, 0, 0.2);
   z-index: 3;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   transition: all 0.5s ease;
   cursor: pointer;
+
+  /* &:hover {
+    opacity: 1;
+  } */
 `;
 
 const Container = styled.div`
   flex: 1;
   margin: 5px;
-  min-width: 286px;
+  min-width: 287px;
   height: 350px;
+  background-color: #f5fbfd;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #f5fbfb;
   position: relative;
 
   &:hover ${Info} {
@@ -47,6 +48,7 @@ const Circle = styled.div`
 
 const Image = styled.img`
   height: 75%;
+  object-fit: cover;
   z-index: 2;
 `;
 
@@ -67,11 +69,40 @@ const Icon = styled.div`
   }
 `;
 
-const Product = ({ img }) => {
+const Heart = styled.img`
+  width: 22px;
+  height: 22px;
+  animation: beat 0.7s linear infinite;
+
+  @keyframes beat {
+    0%,
+    100% {
+      transform: scale(1);
+    }
+
+    25% {
+      transform: rotate(2deg);
+    }
+
+    30% {
+      transform: scale(1.3);
+    }
+
+    50% {
+      transform: scale(1.2);
+    }
+
+    70% {
+      transform: scale(1.3);
+    }
+  }
+`;
+
+const Product = ({ item }) => {
   return (
     <Container>
       <Circle />
-      <Image src={img} />
+      <Image src={item.img} />
       <Info>
         <Icon>
           <ShoppingCartOutlined />
@@ -80,7 +111,7 @@ const Product = ({ img }) => {
           <SearchOutlined />
         </Icon>
         <Icon>
-          <FavoriteBorderOutlined />
+          <Heart src="./assets/heart.png" />
         </Icon>
       </Info>
     </Container>
